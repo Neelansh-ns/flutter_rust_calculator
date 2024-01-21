459849846ffa939d76ce00b0648afde1d4a4d03c
neelansh <neelansh@creatorstack.com>
2024-01-22 01:11:03 +0530

    Added dismiss keyboard on tap outside

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_rust_calculator/src/rust/api/simple.dart';
 import 'package:flutter_rust_calculator/src/rust/frb_generated.dart';
 
@@ -48,78 +49,80 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _expressionController,
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.done,
-                onEditingComplete: () {
-                  FocusScope.of(context).unfocus();
-                  _onCalculateTap();
-                },
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Enter an expression',
-                  hintText: 'e.g. 1 + 2 * 3',
+    return KeyboardDismissOnTap(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  controller: _expressionController,
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.done,
+                  onEditingComplete: () {
+                    FocusScope.of(context).unfocus();
+                    _onCalculateTap();
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter an expression',
+                    hintText: 'e.g. 1 + 2 * 3',
+                  ),
+                  onChanged: (value) {
+                    if (_error != null || _result != null) {
+                      setState(() {
+                        _error = null;
+                        _result = null;
+                      });
+                    }
+                  },
                 ),
-                onChanged: (value) {
-                  if (_error != null || _result != null) {
-                    setState(() {
-                      _error = null;
-                      _result = null;
-                    });
-                  }
-                },
-              ),
-              const SizedBox(height: 24),
-              _error != null
-                  ? Text(
-                      _error!,
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    )
-                  : _result != null
-                      ? Row(
-                          children: [
-                            const Text(
-                              'Result: ',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              _result!,
-                              style: Theme.of(context).textTheme.bodyLarge,
-                            ),
-                          ],
-                        )
-                      : const SizedBox(height: 24),
-              const SizedBox(height: 24),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _onCalculateTap,
-                      child: const Text('Calculate'),
-                    ),
-                    const SizedBox(width: 16),
-                    ElevatedButton(
-                      onPressed: _onClearTap,
-                      child: const Text('Clear'),
-                    ),
-                  ],
+                const SizedBox(height: 24),
+                _error != null
+                    ? Text(
+                        _error!,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )
+                    : _result != null
+                        ? Row(
+                            children: [
+                              const Text(
+                                'Result: ',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                _result!,
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                            ],
+                          )
+                        : const SizedBox(height: 24),
+                const SizedBox(height: 24),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _onCalculateTap,
+                        child: const Text('Calculate'),
+                      ),
+                      const SizedBox(width: 16),
+                      ElevatedButton(
+                        onPressed: _onClearTap,
+                        child: const Text('Clear'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

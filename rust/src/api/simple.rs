68e7ed3ed686780c1f2a4c1a3ab12eec7c451b72
neelansh <neelansh@creatorstack.com>
2024-01-22 00:14:31 +0530

    Added dummy expression to test

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -8,3 +8,11 @@ pub fn init_app() {
     // Default utilities - feel free to customize
     flutter_rust_bridge::setup_default_user_utils();
 }
+
+#[flutter_rust_bridge::frb(sync)]
+pub fn calculate_expression(expression: String) -> Result<String, EvalError<f64>> {
+    let mut c = Computer::<f64>::default();
+    // Add custom functions
+    println!("{}", c.eval("2+2").unwrap());
+
+}

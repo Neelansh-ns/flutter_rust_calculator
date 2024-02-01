251db58936d095ec319620e59b8df260a885bdaf
neelansh <neelansh.s@xoxoday.com>
2024-02-01 13:43:01 +0530

    Added image_picker dependency

---
+++ lib/screens/video_picker_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoPickerScreen extends StatefulWidget {
+  const VideoPickerScreen({super.key});
+
+  @override
+  State<VideoPickerScreen> createState() => _VideoPickerScreenState();
+}
+
+class _VideoPickerScreenState extends State<VideoPickerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
